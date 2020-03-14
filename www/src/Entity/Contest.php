d13451b503adcac4c159837363f54cb04e0c94e9
Hakan Tatlı <hakantatli14@gmail.com>
2020-03-14 16:21:58 +0300

    Contest entity created

---
+++ www/src/Entity/Contest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContestRepository")
+ */
+class Contest
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isCompleted;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsCompleted(): ?bool
+    {
+        return $this->isCompleted;
+    }
+
+    public function setIsCompleted(?bool $isCompleted): self
+    {
+        $this->isCompleted = $isCompleted;
+
+        return $this;
+    }
+}
