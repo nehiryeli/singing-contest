22e4fb4fe286fc0c7d23c86e7d98e6e639d74a4a
Hakan Tatlı <hakantatli14@gmail.com>
2020-03-17 21:20:10 +0300

    namespace correction

--- www/src/Entity/Contest/Contest.php
+++ www/src/Entity/Contest/Contest.php
@@ -11,7 +11,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ContestRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\Contest\ContestRepository")
  */
 class Contest
 {
