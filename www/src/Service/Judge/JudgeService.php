b6aa33c6413458092563cc8e48235c0de4f5ac96
Hakan Tatlı <hakantatli14@gmail.com>
2020-03-17 21:22:48 +0300

    Contestant total score getter

--- www/src/Service/Judge/JudgeService.php
+++ www/src/Service/Judge/JudgeService.php
@@ -4,14 +4,28 @@
 namespace App\Service\Judge;
 
 
+use App\Entity\Contestant\Contestant;
 use App\Entity\Judge\Judge;
 use App\Entity\Judge\JudgeCategory;
+use App\Entity\Judge\MeanJudge;
+use App\Entity\Round\RoundJudgeScore;
 use App\Repository\Judge\JudgeRepository;
+use App\Repository\Round\RoundJudgeScoreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 
 class JudgeService
 {
     const NUMBER_OF_JUDGES = 3; // Number of judges for each contest
+    const JUDGE_NAMESPACE = "App\Entity\Judge\\";
+    const JUDGE_TYPES = array(
+        'RandomJudge',
+        'HonestJudge',
+        'MeanJudge',
+        'RockJudge',
+        'FriendlyJudge'
+    );
+
     /**
      * @var EntityManagerInterface
      */
@@ -20,26 +34,34 @@ class JudgeService
      * @var JudgeRepository
      */
     private $judgeRepository;
+    /**
+     * @var RoundJudgeScoreRepository
+     */
+    private $roundJudgeScoreRepository;
 
     /**
      * JudgeService constructor.
      * @param EntityManagerInterface $entityManager
-     * @param JudgeRepository $judgeRepository
+     * @param RoundJudgeScoreRepository $roundJudgeScoreRepository
      */
-    public function __construct(EntityManagerInterface $entityManager, JudgeRepository $judgeRepository)
+    public function __construct(EntityManagerInterface $entityManager, RoundJudgeScoreRepository $roundJudgeScoreRepository)
     {
         $this->entityManager = $entityManager;
-        $this->judgeRepository = $judgeRepository;
+        $this->roundJudgeScoreRepository = $roundJudgeScoreRepository;
 
     }
 
     public function createJudges()
     {
-        $judges =  array();
+        $judges =  new ArrayCollection();
+
         // loop NUMBER_OF_JUDGES times
         foreach(range(1, self::NUMBER_OF_JUDGES) as $index){
-            $judge = new Judge();
-            $judge->setCategoryId($this->getRandomCategory());
+
+            // Get random judge type from JUDGE_TYPES and concatenate it with JUDGE_NAMESPACE
+            $judgeClass = self::JUDGE_NAMESPACE.$this->getRandomType();
+            $judge =  new $judgeClass;
+
             $this->entityManager->persist($judge);
             $judges[] = $judge;
         }
@@ -48,21 +70,25 @@ class JudgeService
     }
 
 
-
-    public function getJudgeCategories()
+    private function getRandomType()
     {
-        $judgeCategoryRepository = $this->entityManager->getRepository(JudgeCategory::class);
-        return $judgeCategoryRepository->findAll();
-
+        // return random single judge type
+        return  self::JUDGE_TYPES[rand(0, count(self::JUDGE_TYPES)-1)];
     }
 
-    public function getRandomCategory()
+    public function getTotalPointOfContestant(Contestant $contestant) : int
     {
-        $judgeCategoryRepository = $this->entityManager->getRepository(JudgeCategory::class);
-        $judgeCategories = $judgeCategoryRepository->findAll();
 
-        // return random judge categories from the judge categories
-        return  $judgeCategories[rand(0, count($judgeCategories)-1)];
+        $rounds = $this->roundJudgeScoreRepository->findBy(array('contestant' => $contestant));
+        /** @var RoundJudgeScore $round */
+        $total = 0;
+        foreach($rounds as $round){
+            $total += $round->getScore();
+        }
+
+
+        return $total;
+
     }
 
 
